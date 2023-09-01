fe3f6a675120c7bfe804e2acf9c94b4139aa1127
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2023-09-01 23:21:15 +0900

    無限スクロールの実装

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -2,7 +2,7 @@
 
 Kaminari.configure do |config|
   # 1ページ10件に変更する
-  config.default_per_page = 10
+  config.default_per_page = 15
   # config.max_per_page = nil
   # config.window = 4
   # config.outer_window = 0
